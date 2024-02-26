855ba2fc68b6e1cb66e7111ce669876cf049f7a5
Gergely Nagy <152151245+ngergely0@users.noreply.github.com>
2024-02-26 14:11:38 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace SzamitastechnikaiBolt
         {
             public string nev;
             public int darabszam;
-            public int arDollarban;
+            public int ar;
             public string kategoria;
             public string[] muszakiParameterek = new string[3];
         }
@@ -32,10 +32,10 @@ namespace SzamitastechnikaiBolt
                 Bolt adat = new Bolt();
                 adat.nev = sorok[0];
                 adat.darabszam = Convert.ToInt32(sorok[1]);
-                adat.arDollarban = Convert.ToInt32(sorok[2]);
+                adat.ar = Convert.ToInt32(sorok[2]);
                 adat.kategoria = sorok[3];
                 string[] sor = sorok[4].Split('#');
-                for (int i = 0; i < 3; i++)
+                for (int i = 0; i < 2; i++)
                 {
                     adat.muszakiParameterek[i] = sor[i];
 
@@ -44,7 +44,10 @@ namespace SzamitastechnikaiBolt
             }
             sr.Close();
 
-            Console.WriteLine(adatok.Count);
+            for (int i = 0; i < adatok.Count; i++)
+            {
+                Console.WriteLine("Név: {0}\t Darabszám: {1}\t Ár: {2}\t Kategória: {3}\t Paraméterek: {4}", adatok[i].nev, adatok[i].darabszam, adatok[i].ar, adatok[i].kategoria, adatok[i].muszakiParameterek[0]);
+            }
         }
     }
 }
