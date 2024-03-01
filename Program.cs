bcc72f7c15760d5ec445ad87c38fa3f11ada56ee
Gergely Nagy <152151245+ngergely0@users.noreply.github.com>
2024-03-01 13:47:32 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics.Tracing;
@@ -17,6 +18,8 @@ namespace SzamitastechnikaiBolt
     {
         static int kivalasztott;
         static string[] kategoriak = { "Okostelefon", "Egér", "Nyomtató", "Laptop", "Billentyűzet" };
+        static string[] eszkozok = new string[5];
+        static bool megnyomott = false;
         class Bolt
         {
             public string nev;
@@ -25,31 +28,101 @@ namespace SzamitastechnikaiBolt
             public string kategoria;
             public string[] muszakiParameterek = new string[3];
 
-            static void Hozzaadas()
+            public static void Hozzaadas(List<Bolt> adatok)
             {
-                return;
+
+                Bolt ujAdat = new Bolt();
+
+                Console.Write("Név: ");
+                ujAdat.nev = Console.ReadLine();
+
+                Console.Write("Darabszám: ");
+                ujAdat.darabszam = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Ár: ");
+                ujAdat.ar = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Kategória: ");
+                ujAdat.kategoria = Console.ReadLine();
+
+                Console.WriteLine("Műszaki paraméterek: ");
+
+                Console.Write("1.: ");
+                ujAdat.muszakiParameterek[0] = Console.ReadLine();
+
+                Console.Write("2.: ");
+                ujAdat.muszakiParameterek[1] = Console.ReadLine();
+
+                Console.Write("3.: ");
+                ujAdat.muszakiParameterek[2] = Console.ReadLine();
+
+                adatok.Add(ujAdat);
             }
 
-            static void Torles()
+            public static void Torles(int kivalasztott, List<Bolt> adatok)
             {
-                return;
-            }
-            static void Modositas()
-            {
-                return;
-            }
-            static void Vasarlas()
-            {
-                return;
+                adatok.RemoveAt(kivalasztott);
             }
 
-           
+            public static void Modositas(int kivalasztott, List<Bolt> adatok)
+            {
+                Console.Write("Név: ");
+                string modositottNev = Console.ReadLine();
+                adatok[kivalasztott].nev = modositottNev;
+
+                Console.Write("Ár: ");
+                int modositottAr = Convert.ToInt32(Console.ReadLine());
+                adatok[kivalasztott].ar = modositottAr;
+
+                Console.Write("Darabszám: ");
+                int modositottDarabszam = Convert.ToInt32(Console.ReadLine());
+                adatok[kivalasztott].darabszam = modositottDarabszam;
+
+                Console.WriteLine("Műszaki paraméterek: ");
+
+                Console.Write("1.: ");
+                string modositottParameter = Console.ReadLine();
+                adatok[kivalasztott].muszakiParameterek[0] = modositottParameter;
+
+                Console.Write("2.: ");
+                modositottParameter = Console.ReadLine();
+                adatok[kivalasztott].muszakiParameterek[1] = modositottParameter;
+
+                Console.Write("2.: ");
+                modositottParameter = Console.ReadLine();
+                adatok[kivalasztott].muszakiParameterek[2] = modositottParameter;
+
+                Console.WriteLine("Sikeres módosítás!");
+            }
+
+            public static void Vasarlas(int kivalasztott, List<Bolt> adatok)
+            {
+                Console.Write("Mennnyiség: ");
+                int vasaroltdb = Convert.ToInt32(Console.ReadLine());
+
+                int raktaron = adatok[kivalasztott].darabszam;
+
+                if (raktaron > vasaroltdb)
+                {
+                    raktaron = raktaron - vasaroltdb;
+                    Console.WriteLine("A vásárlás sikreres.");
+                }
+                else
+                {
+                    Console.WriteLine("A vásárlás sikertelen.");
+                }
+                adatok[kivalasztott].darabszam = raktaron;
+
+            }
+
+
 
         }
 
-        static void Menu(List<Bolt> adatok, string[,] p_matrix)
+        static void Menu(List<Bolt> adatok)
         {
             kivalasztott = 0;
+            
 
             #region Menü
             ConsoleKeyInfo lenyomott;
@@ -71,7 +144,15 @@ namespace SzamitastechnikaiBolt
                     {
                         Console.ForegroundColor = ConsoleColor.White;
                     }
-                    Console.WriteLine("\t" + (i + 1) + ") " + kategoriak[i]);
+                    if (megnyomott == false) 
+                    {
+                        Console.WriteLine("\t" + (i + 1) + ") " + kategoriak[i]);
+                    }
+                    else if (megnyomott == true)
+                    {
+                        Console.WriteLine("\t" + (i + 1) + ") " + eszkozok[i]);
+                    }
+                   
                 }
                 #endregion
 
@@ -92,39 +173,26 @@ namespace SzamitastechnikaiBolt
             #endregion
             Console.Clear();
 
-            List<Bolt> lista = new List<Bolt>();
-            Bolt termek = new Bolt();
-
+            megnyomott = true;
+            int index = 0;
 
             for (int i = 0; i < adatok.Count; i++)
             {
                 if (kategoriak[kivalasztott] == adatok[i].kategoria)
                 {
-                    termek.nev = adatok[i].nev;
-                    termek.darabszam = adatok[i].darabszam;
-                    termek.ar = adatok[i].ar;
-                    termek.kategoria = adatok[i].kategoria;
-                    termek.muszakiParameterek = adatok[i].muszakiParameterek;
-
-                    lista.Add(termek);
-    
+                    eszkozok[index] = adatok[i].nev;
+                    index++;
                 }
 
             }
 
-            string[,] matrix = new string[5, 5];
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int i = 0; i < eszkozok.Length; i++)
             {
-                matrix[i, 0] = lista[i].nev;
-                matrix[i, 1] = lista[i].darabszam.ToString();
-                matrix[i, 2] = lista[i].ar.ToString();
-                matrix[i, 3] = lista[i].kategoria;
-                matrix[i, 4] = lista[i].muszakiParameterek[0];
-
+                Console.WriteLine(eszkozok[i]);
             }
 
 
-            Menu(string[,] matrix);
+            Menu(adatok);
 
             Thread.Sleep(5000);
 
@@ -157,10 +225,29 @@ namespace SzamitastechnikaiBolt
 
 
             Menu(adatok);
-            Console.WriteLine(kivalasztott);
 
+            /*
+            Console.WriteLine(adatok[0].darabszam);
+            Bolt.Vasarlas(0, adatok);
+            Console.WriteLine(adatok[0].darabszam);
+            */
 
+            /*
+            Console.WriteLine("Név: {0} \t Ár: {1} \t Darabszám: {2} \t Paraméterek: {3} {4} {5}", adatok[0].nev, adatok[0].ar, adatok[0].darabszam, adatok[0].muszakiParameterek[0], adatok[0].muszakiParameterek[1], adatok[0].muszakiParameterek[2]);
+            Bolt.Modositas(0, adatok);
+            Console.WriteLine("Név: {0} \t Ár: {1} \t Darabszám: {2} \t Paraméterek: {3} {4} {5}", adatok[0].nev, adatok[0].ar, adatok[0].darabszam, adatok[0].muszakiParameterek[0], adatok[0].muszakiParameterek[1], adatok[0].muszakiParameterek[2]);
+            */
 
+            /*
+            Bolt.Torles(0, adatok);
+            Bolt.Hozzaadas(adatok);
+
+            for (int i = 0;i < adatok.Count; i++)
+            {
+                Console.WriteLine("Név: {0}  Ár: {1} Darabszám: {2} Paraméterek: {3} {4} {5}", adatok[i].nev, adatok[i].ar, adatok[i].darabszam, adatok[i].muszakiParameterek[0], adatok[i].muszakiParameterek[1], adatok[i].muszakiParameterek[2]);
+            }
+
+            */
 
         }
     }
